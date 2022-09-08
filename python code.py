033cd56b2b027c29fd945e4df3f9b0f89d0953ed
AanchalYadav15 <113080229+AanchalYadav15@users.noreply.github.com>
2022-09-08 08:57:42 +0530

    Add files via upload

---
+++ python code.py
@@ -0,0 +1,7 @@
+#code to check eligibility to vote
+
+age = int(input("Enter a age = "))
+if age >= 18:
+    print("Yes, You are eligible to vote")
+else:
+    print("Sorry, You are not eligible to vote")
